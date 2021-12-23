7360ce02baa734aea42287645b82b7a873f30669
Siddharth Singh <1906211@kiit.ac.in>
2021-12-24 01:32:25 +0530

    db workig

--- app/src/main/java/com/siddharthsinghbaghel/prepopulatedroomdb/MainActivity.kt
+++ app/src/main/java/com/siddharthsinghbaghel/prepopulatedroomdb/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.siddharthsinghbaghel.prepopulatedroomdb.room.QuoteViewModel
 import com.siddharthsinghbaghel.prepopulatedroomdb.room.entities.Quote
+import com.siddharthsinghbaghel.prepopulatedroomdb.room.entities.Record
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -17,6 +18,9 @@ class MainActivity : AppCompatActivity() {
      private var i:Int = -1
      private var red: Boolean = false
      private var green: Boolean = false
+     private var done: Boolean = false
+     private var undone: Boolean = true
+     private var bookmarked: Boolean = false
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,53 +36,101 @@ class MainActivity : AppCompatActivity() {
              quoteList.addAll(it)
         }
 
-       btnN.setOnClickListener{
+
+        btnN.setOnClickListener{
 
            if(i<quoteList.size-1)
            {
                i++
                txt.text = quoteList[i].text
-               doneQuoteReading(quoteList[i].id)
-
+               checkRecord(quoteList[i].id)
            }else{
                Toast.makeText(this,"End",LENGTH_SHORT).show()
            }
        }
+
         btnP.setOnClickListener{
             if(i>0)
             {
                 i--
                 txt.text = quoteList[i].text
+                checkRecord(quoteList[i].id)
+
             }else{
                 Toast.makeText(this,"End",LENGTH_SHORT).show()
             }
         }
 
+
+
         imgBook.setOnClickListener{
- 
-            if(!red){
+            bookmark()
+        }
+
+            imgDone.setOnClickListener{
+                if(!green){
+                    imgDone.setImageResource(R.drawable.ic_baseline_done_all_24)
+                    green = true
+                }else{
+                    imgDone.setImageResource(R.drawable.ic_baseline_done_all_un)
+                    green = false
+                }
+            }
+        }
+
+    private fun checkRecord(id: Int) {
+
+        val recordList = ArrayList<Record>()
+        viewModel.allRecords.observe(this){
+            recordList.addAll(it)
+        }
+
+        val currentIndex = searchList(id,recordList)
+
+        if(currentIndex!=-1){
+
+        }else{
+            doneQuoteReading(quoteList[i].id)
+        }
+
+    }
+
+    private fun searchList(id: Int, recordList: ArrayList<Record>): Int {
+        var j = 0
+        val size = recordList.size
+        var res: Int = -1
+        while(j<size)
+        {
+            if(recordList[j].id == id)
+            {
+                res = j
+                break
+            }else{
+                j++;
+            }
+        }
+        return res
+    }
+
+    private fun bookmark() {
+           if(!red){
                 imgBook.setImageResource(R.drawable.ic_baseline_bookmark_diff)
                 red = true
             }else{
                 imgBook.setImageResource(R.drawable.ic_baseline_bookmark_24)
                 red = false
             }
-        }
-
-        imgDone.setOnClickListener{
-            if(!green){
-                imgDone.setImageResource(R.drawable.ic_baseline_done_all_24)
-                green = true
-            }else{
-                imgDone.setImageResource(R.drawable.ic_baseline_done_all_un)
-                green = false
-            }
-        }
-
-
     }
 
-   private fun doneQuoteReading(id: Int) {
-        TODO("Not yet implemented")
+    private fun doneQuoteReading(id: Int) {
+
+       val recordId: Int = id
+       val newRecord = Record(id, done = true, undone = false, bookmark = false)
+
+        viewModel.insertRecord(newRecord)
+
     }
 }
+
+
+
--- app/src/main/java/com/siddharthsinghbaghel/prepopulatedroomdb/room/entities/Record.kt
+++ app/src/main/java/com/siddharthsinghbaghel/prepopulatedroomdb/room/entities/Record.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "record")
 data class Record(
     @PrimaryKey(autoGenerate = true)
-    val id: Int,
+    var id: Int,
     val done: Boolean,
     val undone: Boolean,
     val bookmark: Boolean
